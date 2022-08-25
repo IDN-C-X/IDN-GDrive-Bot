2f84416fa5a837933c0c2ec1d1a33b7957583530
ZERO • アクバル <ryomensukuna83@gmail.com>
2022-08-26 02:12:11 +0700

    Update utils.py

--- gdrive/plugins/utils.py
+++ gdrive/plugins/utils.py
@@ -1,3 +1,5 @@
+"""utilities plugins."""
+
 import os
 import shutil
 
@@ -7,28 +9,29 @@ from sys import executable
 
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, RPCError
+from pyrogram.types import Message 
 
 from gdrive import SUDO_USERS, DOWNLOAD_DIRECTORY, LOGGER
 
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['log']) & filters.user(SUDO_USERS), group=2)
-def _send_log(client, message):
+def _send_log(client: Client, message: Message):
   with open('log.txt', 'rb') as f:
     try:
-      client.send_document(
-        message.chat.id,
+      await client.send_document(
+        chat_id=message.chat.id,
         document=f,
         file_name=f.name,
-        reply_to_message_id=message.message_id
+        reply_to_message_id=message.id
         )
       LOGGER.info(f'Log file sent to {message.from_user.id}')
     except FloodWait as e:
-      sleep(e.x)
+      sleep(e.value)
     except RPCError as e:
-      message.reply_text(e, quote=True)
+      await message.reply_text(e, quote=True)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['restart']) & filters.user(SUDO_USERS), group=2)
-def _restart(client, message):
+def _restart(client: Client, message: Message):
   shutil.rmtree(DOWNLOAD_DIRECTORY)
   LOGGER.info('Deleted DOWNLOAD_DIRECTORY successfully.')
   message.reply_text('**♻️Restarted Successfully !**', quote=True)
