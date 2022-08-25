5497851844025cb727e42edcb605230bee993a84
ZERO • アクバル <ryomensukuna83@gmail.com>
2022-08-26 02:17:23 +0700

    Update copy.py

--- gdrive/plugins/copy.py
+++ gdrive/plugins/copy.py
@@ -1,4 +1,7 @@
+"""copy plugins."""
+
 from pyrogram import Client, filters
+from pyrogram.types import Message 
 
 from gdrive import LOGGER
 from gdrive.config import BotCommands, Messages
@@ -6,13 +9,13 @@ from gdrive.helpers.utils import CustomFilters
 from gdrive.helpers.gdrive_utils import GoogleDrive
 
 @Client.on_message(filters.private & filters.incoming & filters.command(BotCommands.Clone) & CustomFilters.auth_users)
-def _clone(client, message):
+def _clone(client: Client, message: Message):
   user_id = message.from_user.id
   if len(message.command) > 1:
     link = message.command[1]
     LOGGER.info(f'Copy:{user_id}: {link}')
-    sent_message = message.reply_text(Messages.CLONING.format(link), quote=True)
+    sent_message = await message.reply_text(Messages.CLONING.format(link), quote=True)
     msg = GoogleDrive(user_id).clone(link)
-    sent_message.edit(msg)
+    await sent_message.edit(msg)
   else:
-    message.reply_text(Messages.PROVIDE_GDRIVE_URL.format(BotCommands.Clone[0]))
+    await message.reply_text(Messages.PROVIDE_GDRIVE_URL.format(BotCommands.Clone[0]))
